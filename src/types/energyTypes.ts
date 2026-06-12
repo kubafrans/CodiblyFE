7733984c0999b6590947dbc5adcce540992ccda9
KubaFrans <74661671+KubaFrans@users.noreply.github.com>
2026-06-12 20:13:36 +0200

    (FIX) fixed map error

--- src/types/energyTypes.ts
+++ src/types/energyTypes.ts
@@ -1,5 +1,9 @@
+import type {JSX} from "react/jsx-runtime";
+
 export interface EnergyMixDay {
     date: string;
     cleanEnergyPercentage: number;
     energySources: Record<string, number>;
+
+    map(arg0: (day: any) => JSX.Element): import("react").ReactNode;
 }
